c582a3d9286d5cc83865133dc797ad3b9fa46d10
杨小样随笔 <yang_xuefan@126.com>
2023-08-16 15:30:17 +0800

    静态资源创建 v20230816

--- rabbitFresh/src/main.js
+++ rabbitFresh/src/main.js
@@ -1,4 +1,5 @@
-import './assets/main.css'
+// 引入初始化样式文件
+import '@/style/common.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
--- rabbitFresh/src/router/index.js
+++ rabbitFresh/src/router/index.js
@@ -1,12 +1,35 @@
+// createRouter：创建路由实例
+// createWebHistory：创建History模式的路由
 import { createRouter, createWebHistory } from 'vue-router'
+
+import Login from '@/views/Login/index.vue'
+import Layout from '@/views/Layout/index.vue'
+import Home from '@/views/Home/index.vue'
+import Category from '@/views/Category/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // 配置path和component对应关系
   routes: [
     {
-      // path: '/',
-      // name: 'home',
-      // component: HomeView
+      path: '/',
+      name: 'layout',
+      component: Layout,
+      children: [
+        {
+          path: '',
+          component: Home
+        },
+        {
+          path: 'category',
+          component: Category
+        }
+      ]
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
