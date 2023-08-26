4173f162983eb16e9a6074cf87b24b3786bc708d
杨小样随笔 <yang_xuefan@126.com>
2023-08-26 12:48:10 +0800

    home-product 产品区域渲染(居家，美食，服饰，母婴) v20230826

--- rabbitFresh/src/api/home.js
+++ rabbitFresh/src/api/home.js
@@ -20,3 +20,10 @@ export function getHotAPI() {
     url: '/home/hot'
   })
 }
+
+// 获取所有商品模块
+export const getGoodsAPI = () => {
+  return httpInstance({
+    url: '/home/goods'
+  })
+}
--- rabbitFresh/src/directives/index.js
+++ rabbitFresh/src/directives/index.js
@@ -10,11 +10,15 @@ export const lazyPlugin = {
         // el：指令绑定的那个元素
         // binding：binding.value 指令等于号后面绑定表达式的值
         // console.log(el, binding.value)
-        useIntersectionObserver(el, ([{ isIntersecting }]) => {
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
           if (isIntersecting) {
             // isIntersecting 是一个bool类型，为true时候证明图片进入可视区域
             // 进入可视区就需要请求图片地址
             el.src = binding.value
+            // console.log(isIntersecting)
+            // 图片加载完成后停止监听，否则useIntersectionObserver函数是反复监听的isIntersecting是反复打印true和false
+            // 反复监听会造成资源浪费
+            stop()
           }
         })
       }
